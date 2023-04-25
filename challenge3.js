c3de25360f30c57e02053ea945129f2b9f6b5f9d
serine-18 <127625117+serine-18@users.noreply.github.com>
2023-04-26 00:18:27 +0100

    Add files via upload

---
+++ challenge3.js
@@ -0,0 +1,15 @@
+const dayInp = document.getElementById("day");
+const monthInp = document.getElementById("month");
+const yearInp = document.getElementById("year");
+
+const dayOtp = document.getElementById("DD");
+const monthOtp = document.getElementById("MM");
+const yearOtp = document.getElementById("YY");
+
+const form = document.querySelector("form");
+
+const date = new Date();
+let day = date.getDate();
+let month = 1 + date.getMonth();
+let year = date.getFullYear();
+
